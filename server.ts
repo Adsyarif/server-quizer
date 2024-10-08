23c7d828c62432c2b086a92112294b7fd9a5a622
Adrisa <adrisansyarif@gmail.com>
2024-10-09 05:27:13 +0700

    feat: add cors

--- server.ts
+++ server.ts
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import session from "express-session";
 import userRoute from "./src/routes/user";
+import cors from "cors";
 
 dotenv.config({ path: "config.env" });
 
@@ -13,13 +14,23 @@ const PORT: number = Number(process.env.PORT) || 8080;
 connectDB();
 
 app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD"],
+    credentials: true,
+  })
+);
 
 app.use(
   session({
     secret: process.env.SESSION_SECRET || "my-secret-key",
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: process.env.NODE_ENV === "production" },
+    cookie: {
+      secure: process.env.NODE_ENV === "production",
+      httpOnly: true,
+    },
   })
 );
 
